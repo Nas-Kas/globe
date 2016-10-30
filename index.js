8e3f4923558613dab287967a0e468146b1e7ad2e
Nasir-Kasumu <naskas1234@gmail.com>
2016-10-30 15:43:59 -0400

    change procfile to index.js

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
-var express = require('express');
+///var express = require('express');
 
-var app = express ();
+///var app = express ();
 
 //setup a path to a file
 //app.use ("/static",express.static(__dirname + "webgl-globe-master"));
-app.get('/', function(req,res){
-	res.send('Hello World!');
-});
+///app.get('/', function(req,res){
+	///res.send('Hello World!');
+///});
 //port #
 //app.listen(3000,function(request,response){
-app.listen(3000,function(){
-	console.log("listening on port 3000");
-});
+///app.listen(3000,function(){
+	///console.log("listening on port 3000");
+///});
 
 //npm install express
 // node index.js should run your server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octect-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
